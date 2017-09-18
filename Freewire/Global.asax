b23160ab7deaf3aee67390f9dbfc0f1873af3911
nhering <nhering1@gmail.com>
2017-09-18 09:42:10 -0700

    Add project files.

---
+++ Freewire/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Freewire.MvcApplication" Language="C#" %>
---
+++ Freewire/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Freewire.Startup))]
+namespace Freewire
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
