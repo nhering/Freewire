070451cdd87a41a413dffca2534556d9b2056886
nhering <nhering1@gmail.com>
2017-09-18 09:48:28 -0700

    Add dbcontext and equipment model

---
+++ Freewire/DAL/FWContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Freewire.Models;
+using System.Data.Entity;
+
+namespace Freewire.DAL
+{
+    public class FWContext : ApplicationDbContext
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+
+        public DbSet<EquipmentModel> Equipment { get; set; }
+    }
+}
--- Freewire/Models/EquipmentModel.cs
+++ Freewire/Models/EquipmentModel.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Freewire.Models
 {
     public class EquipmentModel
     {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Manufacturer")]
+        public string Make { get; set; }
+        public string Model { get; set; }
+        [Display(Name = "Max Distance (miles)")]
+        public int Distance { get; set; }
+        [Display(Name = "Max Bandwidth (Mbps)")]
+        public int Bandwidth { get; set; }
+        [Required]
+        public License? License { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public int Cost { get; set; }
+    }
+
+    public enum License
+    {
+        [Display(Name = "Required")]
+        Required,
+        [Display(Name = "Not Required")]
+        NotRequired,
+        [Display(Name = "N/A")]
+        NotApplicable
     }
 }
